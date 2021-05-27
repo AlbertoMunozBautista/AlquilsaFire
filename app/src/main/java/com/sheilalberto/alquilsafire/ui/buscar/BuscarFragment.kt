a89a230ca5066fc6a0e675aed3bf89b7a692c269
AlbertoM <albertomunozbautista@gmail.com>
2021-05-27 12:52:49 +0200

    Firebase

---
+++ app/src/main/java/com/sheilalberto/alquilsafire/ui/buscar/BuscarFragment.kt
@@ -0,0 +1,24 @@
+package com.sheilalberto.alquilsafire.ui.buscar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.sheilalberto.alquilsafire.R
+
+class BuscarFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+       
+        return root
+    }
+}
