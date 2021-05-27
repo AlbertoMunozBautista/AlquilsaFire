8dce939d24f0b898fa7e2b2377822ec24c8fbb68
AlbertoM <albertomunozbautista@gmail.com>
2021-05-27 13:23:58 +0200

    Diseño, mapas y permisos

---
+++ app/src/main/java/com/sheilalberto/alquilsafire/ui/chat/ChatFragment.kt
@@ -0,0 +1,25 @@
+package com.sheilalberto.alquilsafire.ui.chat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.sheilalberto.alquilsafire.R
+
+class ChatFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return root
+    }
+}
--- app/src/main/java/com/sheilalberto/alquilsafire/ui/miUbicacion/MiUbicacionFragment.kt
+++ app/src/main/java/com/sheilalberto/alquilsafire/ui/miUbicacion/MiUbicacionFragment.kt
@@ -1,4 +1,4 @@
-package com.sheilalberto.alquilsafire.ui.home
+package com.sheilalberto.alquilsafire.ui.miUbicacion
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,22 +10,16 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sheilalberto.alquilsafire.R
 
-class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
+class MiUbicacionFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+
         return root
     }
 }
